d330f2e97c75b946717160a780432bf5ec2502f0
Abhi <abhishek7340128579@gmail.com>
2021-05-06 10:28:55 +0530

    Update

--- app/src/main/java/com/example/intentsplayground/IntentPlaygroundActivity.java
+++ app/src/main/java/com/example/intentsplayground/IntentPlaygroundActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -13,36 +14,48 @@ import com.example.intentsplayground.databinding.ActivityIntentPlaygroundBinding
 
 public class IntentPlaygroundActivity extends AppCompatActivity {
     private static final int REQUEST_COUNT = 100;
+
     ActivityIntentPlaygroundBinding b;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Initialize binding
         setupLayout();
 
+        //Initialize Explicit Intent
         setupExplicitIntent();
 
+        //Initialize Implicit Intent
         setupImplicitIntent();
 
+        //Send data
         sendDataToMainActivity();
     }
 
-
+    /**
+     * Send data to MainActivity by using explicit intent
+     */
     private void sendDataToMainActivity() {
+        //click event
         b.btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String sendData = b.sendData.getText().toString().trim();
 
+                //Check data
                 if (sendData.isEmpty()) {
                     b.sendData.setError("Please enter data");
                     return;
                 }
+                //Put data in bundle
                 Bundle bundle = new Bundle();
                 bundle.putInt(Constants.INITIAL_DATA, Integer.parseInt(sendData));
                 bundle.putInt(Constants.MIN_VALUE, 0);
                 bundle.putInt(Constants.MAX_VALUE, 100);
 
+                //Send data by using explicit intent
                 Intent intent = new Intent(IntentPlaygroundActivity.this, MainActivity.class);
                 intent.putExtras(bundle);
                 startActivityForResult(intent, REQUEST_COUNT);
@@ -51,43 +64,57 @@ public class IntentPlaygroundActivity extends AppCompatActivity {
         });
     }
 
-    //Initialize Layout
+    /**
+     * Initialize Layout
+     */
     private void setupLayout() {
         b = ActivityIntentPlaygroundBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
+
         setTitle("Intents Playground");
     }
 
 
-    //Explicit Intent
+    /**
+     * Open MainActivity by using Explicit Intent
+     */
     private void setupExplicitIntent() {
+        //click event
         b.sendExplicitIntent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(IntentPlaygroundActivity.this, MainActivity.class));
-
             }
         });
     }
 
 
-    //Implicit Intent
+    /**
+     * Implicit Intent
+     */
     private void setupImplicitIntent() {
         b.sendImplicitIntent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String str = b.data.getText().toString().trim();
+
+                //Check validation of data
                 if (str.isEmpty()) {
                     b.data.setError("Please enter data");
                     return;
                 }
                 int id = b.radioGroup.getCheckedRadioButtonId();
+
                 if (id == R.id.open_webPage) {
+                    //To open web page
                     openWebPage(str);
                 } else if (id == R.id.dial_no) {
+                    //To open dialer
                     openDialer(str);
 
                 } else if (id == R.id.share_text) {
+                    //To share text
                     openShareMenu(str);
                 } else {
                     Toast.makeText(IntentPlaygroundActivity.this, "Please Select a option", Toast.LENGTH_SHORT).show();
@@ -98,20 +125,30 @@ public class IntentPlaygroundActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Open all sharing app
+     *
+     * @param text Share text to any sharing app
+     */
     private void openShareMenu(String text) {
         Intent intent = new Intent(android.content.Intent.ACTION_SEND);
 
         intent.setType("text/plain");
-
-
         intent.putExtra(android.content.Intent.EXTRA_TEXT, text);
-        /*Fire!*/
+
         startActivity(Intent.createChooser(intent, "Share text via"));
+        //Hide the error
         hideError();
 
     }
 
+    /**
+     * Open the dialer
+     *
+     * @param number number to called
+     */
     private void openDialer(String number) {
+        //Check number
         if (!number.matches("^\\d{10}$")) {
             b.data.setError("Please enter valid number ");
             return;
@@ -121,8 +158,12 @@ public class IntentPlaygroundActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * @param url which is open on Browser
+     */
     private void openWebPage(String url) {
-        if (!url.matches("^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$")) {
+        //check url
+        if (!url.matches("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")) {
             b.data.setError("Please enter valid url");
             return;
         }
@@ -131,11 +172,18 @@ public class IntentPlaygroundActivity extends AppCompatActivity {
     }
 
 
-    //Utils
+    /**
+     * Used to hide the error showed when Text changes
+     */
     private void hideError() {
         b.data.setError(null);
     }
-
+    /**
+     *
+     * @param requestCode   code of the request made
+     * @param resultCode    code of the result
+     * @param data          data which is coming back in the form of result
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
